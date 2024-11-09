fad5e75663b9f879c9a89444148b0dbc42103fa6
madman7164 <madman428426@gmail.com>
2024-11-10 05:04:31 +0900

    [Boj] 기상캐스터

--- version3.cpp
+++ version3.cpp
@@ -1,3 +1,27 @@
-//
-// Created by god on 2024-11-10.
-//
+#include <iostream>
+using namespace std;
+
+int main() {
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+
+  int rows;
+  int cols;
+  cin >> rows >> cols;
+
+  for (int i = 0; i < rows; i++) {
+    int minutes = -1;
+    for (int j = 0; j < cols; j++) {
+      char cloud_flag;
+      cin >> cloud_flag;
+      if (cloud_flag == 'c') {
+        minutes = 0;
+      }
+      cout << minutes << ' ';
+      if (minutes != -1) {
+        minutes++;
+      }
+    }
+    cout << '\n';
+  }
+}
